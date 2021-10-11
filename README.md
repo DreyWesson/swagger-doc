8dbf738eca942223301a5cd5e9b7a43fd444b763
dreyWesson <dreyWesson@gmail.com>
2021-10-11 07:43:29 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### SWAGGER DOCUMENTATION: REST API.
+
+[Helpful link](https://www.section.io/engineering-education/documenting-node-js-rest-api-using-swagger/)
---
+++ src/docs/servers.js
@@ -0,0 +1,12 @@
+module.exports = {
+  servers: [
+    {
+      url: "http://localhost:4000/", // url
+      description: "Local server", // name
+    },
+    {
+      url: "http://oururl.com/todos", // url
+      description: "Production server", // name
+    },
+  ],
+};
